1aaa61a69db23aaf55c4f928b86c0e08c246dde0
ascndia <fixgmc@gmail.com>
2026-03-09 13:22:58 +0700

    add better support for speed and batching, add anima workflow

--- __init__.py
+++ __init__.py
@@ -1,6 +1,6 @@
-from .spectrum_node import SpectrumSDXL
+from .spectrum_node import SpectrumSDXL, SpectrumSDXLBatch
 
-NODE_CLASS_MAPPINGS = {"SpectrumSDXL": SpectrumSDXL}
-NODE_DISPLAY_NAME_MAPPINGS = {"SpectrumSDXL": "Spectrum Adaptive Forecaster (SDXL)"}
+NODE_CLASS_MAPPINGS = {"SpectrumSDXL": SpectrumSDXL, "SpectrumSDXLBatch": SpectrumSDXLBatch}
+NODE_DISPLAY_NAME_MAPPINGS = {"SpectrumSDXL": "Spectrum Adaptive Forecaster (SDXL)", "SpectrumSDXLBatch": "Spectrum Adaptive Forecaster Batch (SDXL)"}
 
 __all__ = ["NODE_CLASS_MAPPINGS", "NODE_DISPLAY_NAME_MAPPINGS"]
